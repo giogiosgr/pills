8356785bccc8c805c73acb49d0314254496a671a
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-10-27 23:05:24 +0100

    Update Pill.java

--- src/main/java/org/pills/java/model/Pill.java
+++ src/main/java/org/pills/java/model/Pill.java
@@ -1,5 +1,88 @@
 package org.pills.java.model;
 
-public class Pill {
+import java.time.LocalDateTime;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@SuppressWarnings("unused")
+
+@Entity
+@Table(name = "pills")
+public class Pill {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotNull
+	@NotEmpty
+	@Size(min = 2, max = 255)
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@NotNull
+	@NotEmpty
+	@Size(min = 2, max = 5000)
+	@Column(name = "content", columnDefinition = "text")
+	private String content;
+	
+	@CreationTimestamp
+	private LocalDateTime createdAt;
+
+	@UpdateTimestamp
+	private LocalDateTime updatedAt;
+	
+	// getters and setters
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	
 }
