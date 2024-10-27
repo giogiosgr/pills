04fbfb37c363543e1e187ae4239b5c5e8c6f4472
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-10-27 22:10:57 +0100

    Initial commit

---
+++ src/main/java/org/pills/java/PillsApplication.java
@@ -0,0 +1,13 @@
+package org.pills.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pills/java/PillsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pills.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
