6bfd924c0d234df64577e99d14bd726b91907d03
Giovanni Ricciardi <104310904+giogiosgr@users.noreply.github.com>
2024-11-25 23:06:19 +0100

    feat: toggle content visibility on click on pill

--- src/main/resources/static/JS/script.js
+++ src/main/resources/static/JS/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+document.querySelectorAll('div[title="pill"]').forEach((element) => element.addEventListener("click", toggleContentVisibility));
+
+function toggleContentVisibility() {
+	
+	let pillContent = document.getElementById(this.id + "c");
+	
+	pillContent.classList.toggle("d-none");
+
+}
+
